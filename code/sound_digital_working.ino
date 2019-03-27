610eacc13b2be5e33d652f6bff5ac1d507e14f6b
Niko Känsäkoski <44393914+nkansako@users.noreply.github.com>
2019-03-27 16:46:29 +0200

    Add files via upload

---
+++ code/sound_digital_working.ino
@@ -0,0 +1,34 @@
+int led = 13;
+int sound = 9;
+int data;
+int value = 0;
+unsigned long start;
+unsigned long test;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  pinMode(sound, INPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  data = digitalRead(sound);
+  //delay(100);
+  if (data == 1) {
+    start = millis();
+    value += 1;
+    delay(100);
+  }
+  test = millis();
+  if (test - start > 1000){
+    value = 0;
+  }
+  if (value == 3) {
+    digitalWrite(led, HIGH);
+    delay(500);
+    digitalWrite(led, LOW); 
+    value = 0;
+  }
+}
